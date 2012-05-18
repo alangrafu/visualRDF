cf5c632ff8d41661177a8cf15119aedd4940bb24
Alvaro Graves <alvaro@graves.cl>
2012-05-18 05:43:15 -0700

    Now is HTML5 valid

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ if(isset($_GET['url'])){
 <!DOCTYPE html>
 <html>
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <script type="text/javascript" src="js/d3/d3.js"></script>
 <script type="text/javascript" src="js/d3/d3.layout.js"></script>
 <script type="text/javascript" src="js/d3/d3.geom.js"></script>
@@ -21,15 +22,15 @@ var url = '<?=$url?>';
 <h1 style="display: inline"><a href='.'>Visual RDF</a></h1>
 <p style="display:inline; position:absolute"><small><a href='https://github.com/alangrafu/visualRDF'>Github Repository</a></small></p>
   <div style="float:right">
-<div><strong style="color: red">Usage: </strong> <strong>Scroll</strong> ↦ Zoom. <strong>Drag node</strong> ↦ Move node. <strong>Drag background</strong> ↦ Move graph.</div>
-  <form method="get" action="" style='float:left'>
+<div><strong style="color: red">Usage: </strong> <strong>Scroll</strong> &#8594; Zoom. <strong>Drag node</strong> &#8594; Move node. <strong>Drag background</strong> &#8594; Move graph.</div>
+  <form method="get" action="." style='float:left'>
     <input type='text' id='url' name='url' value='<?=$url?>' size='100'/>
     <input type="submit" value="Redraw"/>
   </form><div id="msg"><input type="checkbox" id="properties"/><label>Show properties</label>
   <div style="display:inline"><input type="checkbox" id="hidePredicates"/><label>Hide predicates</label>
                        <div id="preds" style="border: 1px solid black; position:absolute; display:none; color: white; background: rgba(0, 0, 0, 0.6);;"></div>
 </div>
-  <img id="waiting" src="img/waiting.gif"/>
+  <img id="waiting" alt="waiting icon" src="img/waiting.gif"/>
 </div>
 </div>
 </div>
